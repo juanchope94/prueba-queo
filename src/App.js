393040fcd1af350606b8005e928aca6fccbc48b9
juanchope94 <mjuan@unicauca.edu.co>
2020-06-20 15:03:53 -0500

    enrrutando paginas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import history from './history';
 import Dashboard from './pages/main-dash/Dashboard'
 import Login from  './pages/login/login'
+import { Route, Switch, BrowserRouter,withRouter, Redirect } from 'react-router-dom';
 import configureStore from './redux/configureStore'
 const store = configureStore();
 class App extends Component {
@@ -12,7 +12,10 @@ class App extends Component {
     return (
       <Provider store={store}>
         <BrowserRouter history={history}>
-          <Login />
+        <Route exact path="/" render={()=><Redirect to="/login"/>}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/Dashboard" component={Dashboard}/>
+
         </BrowserRouter>
       </Provider>
     );
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -9,7 +9,6 @@ import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Logo from './../../assets/queoLogo.png'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
